fa05006ec7dcee6098d88a0759e718f958532f45
YSLIU627 <zhliu0627@gmail.com>
2025-03-06 23:13:42 -0600

    addscript

--- running_scripts/2-27-2-kl0.01_zeroshot_qwen2-code-1.5b-iter1-grpo-balance.sh
+++ running_scripts/2-27-2-kl0.01_zeroshot_qwen2-code-1.5b-iter1-grpo-balance.sh
@@ -66,6 +66,6 @@ python3 -m verl.trainer.main_ppo_correct \
     trainer.default_local_dir=${SAVE_LOCAL_DIR} \
     trainer.n_gpus_per_node=8 \
     trainer.nnodes=1 \
-    trainer.save_freq=5 \
-    trainer.test_freq=5 \
+    trainer.save_freq=10 \
+    trainer.test_freq=10 \
     trainer.total_epochs=1 $@
